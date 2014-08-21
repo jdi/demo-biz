ea374b3af8241f3e1ec8cf850685fffd6639964a
Brooke Bryan <brooke@bajb.net>
2014-08-21 15:45:31 +0100

    TNT Integration

--- src/Applications/Frontend/Controllers/JoinController.php
+++ src/Applications/Frontend/Controllers/JoinController.php
@@ -3,12 +3,32 @@ namespace DemoCorp\Applications\Frontend\Controllers;
 
 use Cubex\View\LayoutController;
 use DemoCorp\Applications\Frontend\Views\AssetTemplatedView;
+use JDI\TntAffiliate\Models\ActionOptions;
+use JDI\TntAffiliate\TntAffiliate;
+use JDI\TntAffiliate\TntAffiliateApi;
 
 class JoinController extends LayoutController
 {
   public function postDefaultAction()
   {
     //Trigger Join
+    $api    = new TntAffiliateApi();
+    $config = $this->getCubex()->getConfiguration()->getSection('tntaffiliate');
+    $api->login(
+      $config->getItem('client_id'),
+      $config->getItem('client_secret')
+    );
+    $options       = new ActionOptions();
+    $options->data = $this->_getRequest()->request->all();
+    $resp          = $api->triggerAction(
+      'lead',
+      TntAffiliate::getVisitorId(),
+      $options
+    );
+    return [
+      'action_id' => $resp->getActionId(),
+      'pixels'    => $resp->getPixels()
+    ];
   }
 
   public function defaultAction()
--- src/Applications/Frontend/Controllers/PurchaseController.php
+++ src/Applications/Frontend/Controllers/PurchaseController.php
@@ -3,12 +3,32 @@ namespace DemoCorp\Applications\Frontend\Controllers;
 
 use Cubex\View\LayoutController;
 use DemoCorp\Applications\Frontend\Views\AssetTemplatedView;
+use JDI\TntAffiliate\Models\ActionOptions;
+use JDI\TntAffiliate\TntAffiliate;
+use JDI\TntAffiliate\TntAffiliateApi;
 
 class PurchaseController extends LayoutController
 {
   public function postDefaultAction()
   {
     //Trigger Purchase
+    $api    = new TntAffiliateApi();
+    $config = $this->getCubex()->getConfiguration()->getSection('tntaffiliate');
+    $api->login(
+      $config->getItem('client_id'),
+      $config->getItem('client_secret')
+    );
+    $options       = new ActionOptions();
+    $options->data = $this->_getRequest()->request->all();
+    $resp          = $api->triggerAction(
+      'sale',
+      TntAffiliate::getVisitorId(),
+      $options
+    );
+    return [
+      'action_id' => $resp->getActionId(),
+      'pixels'    => $resp->getPixels()
+    ];
   }
 
   public function defaultAction()
