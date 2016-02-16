4e92bcc8968e40be7371751cab044509147bd132
Jleagle <jimeagle@gmail.com>
2016-02-16 11:01:30 +0000

    Fix pixels

--- src/Applications/Frontend/Controllers/JoinController.php
+++ src/Applications/Frontend/Controllers/JoinController.php
@@ -2,38 +2,43 @@
 namespace DemoCorp\Applications\Frontend\Controllers;
 
 use DemoCorp\Applications\Frontend\Views\AssetTemplatedView;
+use Fortifi\FortifiApi\Affiliate\Responses\Pixels\PixelsResponse;
 use Packaged\Helpers\Strings;
 
 class JoinController extends BaseController
 {
-  public function postDefaultAction()
-  {
-    $reqData = $this->_getRequest()->request->all();
-    $eventRef = idx($reqData, 'event_ref', Strings::randomString(10));
-
-    //Trigger Fortifi Join
-    $this->_getFortifi()->customer()->create(
-      'FID:COMP:1429731764:3d9f2a4ed06c',
-      idx($reqData, 'email'),
-      idx($reqData, 'first_name'),
-      null,
-      null,
-      $eventRef
-    );
-
-    echo '<div class="row"><div class="box"><div class="col-lg-12">';
-
-    echo '<h4>Fortifi Pixels</h4>';
-    foreach($this->_getFortifi()->visitor()->getPixels() as $pixel)
-    {
-      echo '<textarea cols="100" rows="6">' . esc($pixel) . '</textarea><br/>';
-    }
-
-    echo '</div></div></div>';
-  }
-
   public function defaultAction()
   {
     return new AssetTemplatedView($this, 'Join');
+  }
+
+  public function postDefaultAction()
+  {
+    $post = $this->_getRequest()->request;
+
+    //Trigger Fortifi Join
+    $this->_getFortifi()->customer()->create(
+      'FID:COMP:1454595603:9HOP5UWRfGwf',
+      $post->get('email'),
+      $post->get('firstname'),
+      null,
+      null,
+      $post->get('event_ref', Strings::randomString(10))
+    );
+
+    echo '<div class="row"><div class="box"><div class="col-lg-12">';
+    echo '<h4>Fortifi Pixels</h4>';
+
+    $pixels = $this->_getFortifi()->visitor()->getPixels();
+    if($pixels instanceof PixelsResponse)
+    {
+      foreach($pixels->items as $pixel)
+      {
+        echo '<textarea cols="100" rows="6">'
+          . Strings::escape($pixel) . '</textarea><br/>';
+      }
+    }
+
+    echo '</div></div></div>';
   }
 }
