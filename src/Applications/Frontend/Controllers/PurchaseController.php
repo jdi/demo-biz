eeadf733594f035caf5d806867359a6e74354949
Tom Kay <oridan82@gmail.com>
2016-02-16 16:34:35 +0000

    improve pixel output

--- src/Applications/Frontend/Controllers/PurchaseController.php
+++ src/Applications/Frontend/Controllers/PurchaseController.php
@@ -25,24 +25,23 @@ class PurchaseController extends BaseController
       false
     );
 
-    // pixels are triggered by JS for purchase
-    // CSS to make the pixel box visible
-    AssetManager::assetType()->requireInlineCss(
-      '#fortifi-px-container { width:initial!important;height:initial!important;position:initial!important;top:initial!important;left:initial!important; }'
+    // pixels are triggered by JS for purchase and inserted into #fortifi-px-container
+    $trackDomain = $this->getConfigItem('fortifi', 'track');
+    AssetManager::assetType()->requireInlineJs(
+      <<<FIN
+(function (d, s, i, u)
+{
+  if (d.getElementById(i)) {return;}
+  var n = d.createElement(s), e = d.getElementsByTagName(s)[0];
+  n.id = i;
+  n.src = '//' + u + '/px/init/fortifi.js';
+  e.parentNode.insertBefore(n, e);
+})(document, "script", "fortifi-pixel", "$trackDomain");
+FIN
     );
-    AssetManager::assetType()->requireInlineJs('(function (d, s, i, u)
-  {
-    if (d.getElementById(i))
-    {return;}
-    var n = d.createElement(s), e = d.getElementsByTagName(s)[0];
-    n.id = i;
-    n.src = \'//\' + u + \'/px/init/fortifi.js\';
-    e.parentNode.insertBefore(n, e);
-  })(
-    document, "script", "fortifi-pixel",
-    "'. $this->getConfigItem('fortifi','track') .'"
-  );
-');
+    echo '<div class="row"><div class="box"><div class="col-lg-12">';
+    echo '<h4>Pixels (JS)</h4><div id="fortifi-px-container"></div>';
+    echo '</div></div></div>';
   }
 
   public function defaultAction()
